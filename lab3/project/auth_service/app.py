44f6c72c7da63f88a5a1f407575c56860403e9a9
dkidot <danielkotof@yandex.ru>
2024-06-11 15:47:34 +0300

    changes for logging

--- lab3/project/auth_service/app.py
+++ lab3/project/auth_service/app.py
@@ -1,12 +1,46 @@
-from flask import Flask, request, render_template, redirect, url_for, session, jsonify
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify, g
 from utils import *
 import pika
 import json
+import time
+import logging
+from logging.handlers import RotatingFileHandler
+import logging.config
+from pythonjsonlogger import jsonlogger
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+handler = RotatingFileHandler('/dummy/auth_service.log', maxBytes=2000, backupCount=10)
+handler.setLevel(logging.DEBUG)
+
+formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.DEBUG)
+log.addHandler(handler)
+
+@app.route("/")
+def main():
+    app.logger.debug("Debug log level")
+    app.logger.info("Program running correctly")
+    app.logger.warning("Warning: low disk space")
+    app.logger.error("Error!")
+    app.logger.critical("Program halt!")
+    return "logger levels"
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.before_request
+def start_timer():
+    g.start = time.time()
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    app.logger.info("Hello from app.py")
+    return "Hello"
+
+
 def register():
     if request.method == "POST":
         if request.is_json:
@@ -16,11 +50,14 @@ def register():
         username = data.get('username')
         password = data.get('password')
         if not username or not password:
+            app.logger.warning("Missing username of password during registration")
             return jsonify({"error": "Missing username or password"}), 400
         if get_user_from_db(username):
+            app.logger.warning("Attempt to register with existing username: {username}")
             return jsonify({"error": "User already exists"}), 400
         hash_passwd, salt = hashing(password, HASH_SALT)
         save_user_to_db(username, hash_passwd, salt)
+        app.logger.info(f"User registered successfully: {username}")
         return jsonify({"message": "User registered successfully"}), 201
     return render_template('register.html')
 
@@ -36,6 +73,7 @@ def login():
         username = data.get('username')
         password = data.get('password')
         if not username or not password:
+            app.logger.warning("Missing username or password during loging")
             return jsonify({"error": 'Missing username of password'}), 400
 
         user = get_user_from_db(username)
@@ -43,10 +81,13 @@ def login():
             db_username, db_password_hash, db_salt = user
             if hashing(password, HASH, db_salt) == db_password_hash:
                 session['username'] = username
+                app.logger.info(f"User logged succesfully: {username}")
                 return redirect(url_for("upload"))
             else:
+                app.logger.warning("Attempt to register with existing username: {username}")
                 return jsonify({"error" : 'Ivalid credentials'}), 401
         else:
+            app.logger.warning(f"User already exists: {username}")
             return jsonify({'error': 'User does not exist'}), 404
     return render_template('login.html')
 
@@ -54,18 +95,20 @@ def login():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if 'username' not in session:
+        app.logger.warning("Unathorized attempt to /upload")
         return redirect(url_for('login'))
 
     if request.method == 'POST':
         if 'file' not in request.files:
+            app.logger.warning("No file part in upload request")
             return jsonify({"error": 'No file part'}), 400
         user_file = request.files['file']
 
         if user_file.filename == '':
+            app.logger.warning("No file part in upload request")
             return jsonify({"error": 'No selected file'}), 400
 
         if user_file and allowed_file(user_file.filename):
-            # Читаем файл и отправляем его в очередь RabbitMQ
             file_content = user_file.read()
             connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
             channel = connection.channel()
@@ -79,8 +122,10 @@ def upload():
 
             channel.basic_publish(exchange='', routing_key='file_queue', body=message)
             connection.close()
+            app.logger.info(f"File uploaded successfully: {user_file.filename} by {session['username']}")
             return jsonify({'message': 'File uploaded successfully'}), 200
         else:
+            app.logger.warning(f"Invalid file upload attempt by {session['username']}")
             return jsonify({'error': 'No file selected'}), 400
     return render_template('upload.html')
 
@@ -88,6 +133,7 @@ def upload():
 @app.route("/logout")
 def logout():
     session.pop('username', None)
+    app.logger.info(f"User logged out: {username}")
     return redirect(url_for('login'))
 
 
@@ -97,4 +143,4 @@ def welcome():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
